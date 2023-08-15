20c126d5b82faf2014bb799001089b6f13d7bb89
Phuong <phuong.zorum24@student.passerellesnumeriques.org>
2023-08-15 09:07:46 +0700

    done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,7 +18,7 @@ function App() {
       <Route path='/home' element={<Content/>} />
       </Switch>
     </Router>
-
+  
    
   );
 }
--- src/components/Contents/Login.js
+++ src/components/Contents/Login.js
@@ -28,28 +28,37 @@ function Login() {
   };
 
   return (
-      <form className='flex flex-col justify-center items-center mt-20 py-2 bg-white shadow-sm'>
-        <h2>Login</h2>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        />
-        <button className='w-1/4 px-3 py-2 font-semibold bg-orange-500 rounded-sm text-white' onClick={handleLogin}>Login</button>
+      <form className="ml-82 mt-20 w-1/3 bg-white shadow-lg mx-auto py-20 px-3">
+        <div className="ml-12">
+            <label className="font-medium">Username</label>
+            <input type="text" className="ml-4 border border-lime-400 px-3 py-2 rounded-lg hover:border-lime-600" 
+              value={username}
+              onChange={e => setUsername(e.target.value)} name="" id="" placeholder="username" />
+        </div>
+        <div className= "mt-4 ml-12">
+            <label className="font-medium">Password</label>
+            <input type="text" className="ml-5 border border-lime-400 px-3 py-2 rounded-lg"
+               value={password}
+               onChange={e => setPassword(e.target.value)}
+               name="" id="" placeholder="password"/>
+        </div>
+        <div className= "mt-4 ml-12">
+            <label className="font-medium">Email</label>
+            <input type="text" className="ml-12 border border-lime-400 px-3 py-2 rounded-lg" 
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            placeholder="email" />
+        </div>
+     
+        <div className='ml-32'>
+        <button className='px-6 py-2 bg-lime-600 text-white ml-18 mt-4 rounded-lg hover:bg-lime-700' onClick={handleLogin}>Login</button>
         {errorMessage && <p>{errorMessage}</p>}
+        </div>
+      
+        <div className="text-gray-400 ml-36 mt-2">
+            <span>Have account yet? </span>
+            <a className="text-blue-600" href="./register">signup</a>
+        </div>
       </form>
 
   );
