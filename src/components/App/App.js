cb1dcd5cd48751e8c640d98b96439f23ac3c417e
phoenixnam <phuong.zorum24@student.passerellesnumeriques.org>
2023-08-30 17:24:16 +0700

    remove react dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,19 +5,13 @@ import Footer from '../Footer/Footer';
 import Content from '../Contents/Coffee';
 import RegisterForm from '../Contents/Register';
 import Login from '../Contents/Login';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 
 
 function App() {
   return (
-    <Router>
-      <Switch>
-      <Route path='/login' element={<Login/>} />
-      <Route path='/register' element={<RegisterForm/>} />
-      <Route path='/home' element={<Content/>} />
-      </Switch>
-    </Router>
+    <Content></Content>
   
    
   );
